ad7eb6621622b7520d03e1e8b749b415298317c0
Samuel Gomez <gocs020101@gs.utm.mx>
2025-04-28 19:14:20 -0600

    Pruebas de la pantalla ejercicio

--- test/test_pantalla_ejercicio.dart
+++ test/test_pantalla_ejercicio.dart
@@ -10,51 +10,49 @@ import 'package:provider/provider.dart';
 import 'package:deepsleep/presentation/controllers/Controllers.dart';
 
 void main() {
-  testWidgets('PantallaEjercicio renders all expected widgets', (WidgetTester tester) async {
-    // Envolver la pantalla con el Provider adecuado
+  testWidgets('PantallaEjercicio todos los widgets son renderizados correctamente', (WidgetTester tester) async {
     await tester.pumpWidget(
       ChangeNotifierProvider(
-        create: (_) => Controllers(), // Aquí se debe crear el Controller para las pruebas
+        create: (_) => Controllers(),
         child: MaterialApp(home: PantallaEjercicio()),
       ),
     );
 
-    // Verificar que el encabezado "Mi ejercicio" esté presente
     expect(find.text('Mi ejercicio'), findsOneWidget);
-
-    // Verificar que el widget de progreso esté presente
     expect(find.byType(ProgresoWidget), findsOneWidget);
-
-    // Verificar que el widget de métricas esté presente
     expect(find.byType(BuildMetricos), findsOneWidget);
-
-    // Verificar que la lista de ejercicios esté presente
     expect(find.byType(BuildListaEjercicios), findsOneWidget);
-
-    // Verificar que el gráfico cardíaco esté presente
     expect(find.byType(BuildGraficoCardiaco), findsOneWidget);
   });
 
-  testWidgets('PantallaEjercicio scrolls correctly', (WidgetTester tester) async {
+  testWidgets('PantallaEjercicio hace scroll correctamente', (WidgetTester tester) async {
     await tester.pumpWidget(
       ChangeNotifierProvider(
-        create: (_) => Controllers(), // Proveedor para los test
+        create: (_) => Controllers(),
         child: MaterialApp(home: PantallaEjercicio()),
       ),
     );
 
-    // Asegúrate de que el widget sea scrollable
     final scrollable = find.byType(SingleChildScrollView);
     expect(scrollable, findsOneWidget);
 
-    // Realizar desplazamiento hacia abajo
     await tester.drag(scrollable, Offset(0, -300));
     await tester.pumpAndSettle();
 
-    // Verifica que los widgets aún sean visibles después de desplazarse
     expect(find.byType(ProgresoWidget), findsOneWidget);
     expect(find.byType(BuildMetricos), findsOneWidget);
     expect(find.byType(BuildListaEjercicios), findsOneWidget);
     expect(find.byType(BuildGraficoCardiaco), findsOneWidget);
+  });
+
+  testWidgets('PantallaEjercicio lista ejercicios correctamente', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      ChangeNotifierProvider(
+        create: (_) => Controllers(),
+        child: MaterialApp(home: PantallaEjercicio()),
+      ),
+    );
+
+    expect(find.byType(BuildExerciseItem), findsWidgets);
   });
 }
